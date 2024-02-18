24de12f87d4295e7169595daea975a9669fe7e23
58sky <58sky2021@gmail.com>
2024-02-18 01:07:14 -0500

    .

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-function 
+function toggleList() {
+    var x = document.getElementById("myList");
+    if (x.style.display === "block" || x.style.display === "") {
+        x.style.display = "none";
+    } else {
+        x.style.display === "block";
+    }
+}
